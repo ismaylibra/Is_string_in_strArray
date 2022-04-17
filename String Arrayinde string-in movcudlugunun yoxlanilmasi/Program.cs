78d4d33a0b158610b1adee15d6b8277e8dbea1fe
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-17 16:21:10 +0400

    added classwork 2 15.04.2022

---
+++ String Arrayinde string-in movcudlugunun yoxlanilmasi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace String_Arrayinde_string_in_movcudlugunun_yoxlanilmasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strArr = { "Asus", "Hp", "DELL", "Acer" };
+            string a = Console.ReadLine();
+            for (int i = 0; i < strArr.Length; i++)
+            {  if( strArr[i] == a)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+    }
+}
